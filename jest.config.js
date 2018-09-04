accd2f311b49a123cf5855a50adfd03f5f6c4ea0
Wen Zhao <wzhao@money-media.com>
2018-09-04 11:49:41 -0400

    Renamed /tests to /test

--- jest.config.js
+++ jest.config.js
@@ -134,7 +134,7 @@ module.exports = {
 
   // The glob patterns Jest uses to detect test files
   testMatch: [
-    "**/tests/**/*.js?(x)"
+    "**/test/**/*.js?(x)"
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
